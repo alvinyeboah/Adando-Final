b8ba3e891f7dea492690aa87f44073db7d2acf57
alvinyeboah <alvinyeboah5@gmail.com>
2023-10-13 21:42:00 +0000

    New

--- index.js
+++ index.js
@@ -4,5 +4,19 @@ const port = 3200
 app.use(express.static('src'))
 
 app.get('/', (req, res) => res.sendFile(__dirname+ "/src/index.html"))
+app.post('/storeMood', (req, res) => {
+  const { mood } = req.body;
+
+  const newMood = new Mood({ mood });
+
+  newMood.save((err, mood) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send('Internal Server Error');
+    }
+
+    res.status(200).send('Mood stored successfully');
+  });
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
