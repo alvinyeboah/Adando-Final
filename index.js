900285e490b5cba46376a3c250889c5a69db29c9
alvinyeboah <alvinyeboah5@gmail.com>
2023-10-13 19:00:04 +0000

    New

--- index.js
+++ index.js
@@ -1,34 +1,8 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const passport = require("passport");
-const session = require("express-session");
+const express = require('express')
+const app = express()
+const port = 3200
+app.use(express.static('src'))
 
-const app = express();
+app.get('/', (req, res) => res.sendFile(__dirname+ "/src/index.html"))
 
-// Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/node_auth'
-  );
-
-// Set up express-session
-app.use(
-  session({
-    secret: "your-secret-key",
-    resave: false,
-    saveUninitialized: false,
-  })
-);
-
-// Initialize passport
-app.use(passport.initialize());
-app.use(passport.session());
-
-// Set up routes
-app.use("/auth", require("./routes/auth"));
-
-app.use(express.static("src"));
-
-// Start server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
-});
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
